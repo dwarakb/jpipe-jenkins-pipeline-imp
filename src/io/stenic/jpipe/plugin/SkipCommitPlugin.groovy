df6a768115348506d6d501ee6c65d21538338340
Dries De Peuter <dries@stenic.io>
2022-05-05 17:47:48 +0200

    feat: Better skip

--- src/io/stenic/jpipe/plugin/SkipCommitPlugin.groovy
+++ src/io/stenic/jpipe/plugin/SkipCommitPlugin.groovy
@@ -5,6 +5,11 @@ import org.jenkinsci.plugins.scriptsecurity.sandbox.RejectedAccessException
 import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 
 class SkipCommitPlugin extends Plugin {
+
+    private Boolean doSkip = false;
+    private Boolean isResolved = false;
+
+
     public Map getSubscribedEvents() {
         return [
             "${Event.PREPARE}": [
@@ -26,9 +31,16 @@ class SkipCommitPlugin extends Plugin {
     }
 
     public Boolean doSkipCommit(Event event, String stageName) {
+
+        if (!this.isResolved) {
+            def commitMsg = event.script.sh(script: "git log -n 1 HEAD", returnStdout: true)
+            this.doSkip = commitMsg.matches(/(?ms)(.*\[(skip ci|ci skip)\].*)/)
+            this.isResolved = true
+        }
+
         // If the last commit includes [ci skip], do not proceed.
-        def commitMsg = event.script.sh(script: "git log -n 1 HEAD", returnStdout: true)
-        if (commitMsg.matches(/(?ms)(.*\[(skip ci|ci skip)\].*)/)) {
+        
+        if (this.doSkip) {
             Utils.markStageSkippedForConditional(stageName)
             // event.script.currentBuild.description = 'Skipped by [skip ci]'
             // event.script.currentBuild.result = event.script.currentBuild.getPreviousBuild().result
